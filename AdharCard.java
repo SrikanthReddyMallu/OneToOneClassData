88b3c5b6f2ca817d44e9b112189b0c4cc7ea9c81
SrikanthReddyMallu <126140093+SrikanthReddyMallu@users.noreply.github.com>
2023-03-08 23:32:51 +0530

    Add files via upload

---
+++ AdharCard.java
@@ -0,0 +1,22 @@
+package com.onetoone;
+
+public class AdharCard 
+{
+private int id;
+private String address;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+
+
+}
